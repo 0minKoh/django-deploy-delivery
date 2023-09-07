8c91fa14c428d2f1d7bdde370ff02309bac7a682
0minKoh <esesang13@naver.com>
2023-09-07 14:28:00 +0900

    wsgi setting

--- cjdelievery/wsgi.py
+++ cjdelievery/wsgi.py
@@ -6,6 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
+# uwsgi --http :[포트번호] --home [가상환경 경로] --chdir [장고프로젝트폴더 경로] -w [wsgi 모듈이 있는 폴더].wsgi
 
 import os
 
