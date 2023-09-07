559aca01208e139e50c227af1d2b07462eb6257b
0minKoh <esesang13@naver.com>
2023-09-07 15:46:08 +0900

    mysite cjdelievery

--- cjdelievery/wsgi.py
+++ cjdelievery/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 # uwsgi --http :[포트번호] --home [가상환경 경로] --chdir [장고프로젝트폴더 경로] -w [wsgi 모듈이 있는 폴더].wsgi
+# sudo /home/ubuntu/myvenv/bin/uwsgi -i /srv/django-deploy-delivery/.config/uwsgi/mysite.ini
 
 import os
 
